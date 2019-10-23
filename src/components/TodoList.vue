a0792368c154c7eaef18a80bfadecf35d660519b
James Grima <james@stevesandco.com>
2019-10-23 13:46:17 +0200

    filters

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
       <input type="text" class="todo-input" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addTodo">
-      <div v-for="(todo, index) in todos" :key="todo.id" class='todo-item'>
+      <div v-for="(todo, index) in todosFiltered" :key="todo.id" class='todo-item'>
           <div class="todo-item-left">
+              <input type="checkbox" v-model="todo.completed">
                 <div 
                 v-if="!todo.editing" 
                 @dblclick="editTodo(todo)" 
-                class="todo-item-label">
+                class="todo-item-label" :class="{ completed : todo.completed }">
 
                 {{todo.title}}
 
@@ -29,6 +30,25 @@
         
           
       </div>
+        <div class="extra-container">
+            <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label></div>
+            <div>{{ remaining }} items left</div>
+        </div>
+
+        <div class="extra-container">
+            <div>
+                <button :class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
+                <button :class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
+                <button :class="{ active: filter == 'completed' }" @click="filter = 'completed'">Completed</button>
+            </div>
+
+            <div>
+                <transition name="fade">
+                <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+                </transition>
+            </div>
+    </div>
+
   </div>
 </template>
 
@@ -40,6 +60,7 @@ export default {
             newTodo: '',
             idForTodo: '4',
             beforeEditCache: '',
+            filter: 'all',
             todos: [
                 {
                     'id': 1,
@@ -93,13 +114,47 @@ export default {
             todo.editing = true
         },
         doneEdit(todo){
+             if(todo.title.trim() == ''){
+                todo.title = this.beforeEditCache;
+            }
             todo.editing = false;
         },
         cancelEdit(todo){
             todo.title = this.beforeEditCache
             todo.editing = false;
 
+        },
+        checkAllTodos(){
+            this.todos.forEach((todo) => todo.completed = event.target.checked)
+        },
+        clearCompleted(){
+            this.todos = this.todos.filter(todo => !todo.completed);
         }
+    },
+    computed: {
+        remaining(){
+            return this.todos.filter(todo => !todo.completed).length;
+        },
+        anyRemaining(){
+            return this.remaining != 0;
+        },
+        todosFiltered(){
+            if(this.filter == 'all'){
+                return this.todos
+            }else if(this.filter == 'active'){
+                return this.todos.filter(todo => !todo.completed)
+            }else if(this.filter == 'completed'){
+                return this.todos.filter(todo => todo.completed)
+            }else{
+                return this.todos
+            }
+
+            
+        },
+        showClearCompletedButton(){
+            return this.todos.filter(todo => todo.completed).length > 0;
+        }
+       
     }
 }
 </script>
