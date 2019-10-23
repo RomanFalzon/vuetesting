a43f7a09de8fc3c22800f77f3a78c62745d1322a
James Grima <james@stevesandco.com>
2019-10-23 12:31:20 +0200

    start

---
+++ src/components/TodoList.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+      <input type="text" class="todo-input" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="addTodo">
+      <div v-for="(todo, index) in todos" :key="todo.id" class='todo-item'>
+          <div class="todo-item-left">
+                <div 
+                v-if="!todo.editing" 
+                @dblclick="editTodo(todo)" 
+                class="todo-item-label">
+
+                {{todo.title}}
+
+                </div>
+
+                <input
+                v-else 
+                class="todo-item-edit" 
+                type="text" 
+                v-model="todo.title"
+                @blur="doneEdit(todo)"
+                @keyup.enter="doneEdit(todo)"
+                @keyup.esc="cancelEdit(todo)"
+                v-focus>
+          </div>
+
+          <div class="remove-item" @click="removeTodo(index)">
+              &times;
+          </div>
+        
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-list',
+    data (){
+        return{
+            newTodo: '',
+            idForTodo: '4',
+            beforeEditCache: '',
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Testing',
+                    'completed' : false,
+                    'editing' : false
+                },
+                {
+                    'id': 2,
+                    'title': 'Testing2',
+                    'completed' : false,
+                    'editing' : false
+                },
+                {
+                    'id': 3,
+                    'title': 'Testing3',
+                    'completed' : true,
+                    'editing' : false
+                }
+            ]
+        }
+    },
+    directives: {
+        focus: {
+            inserted: function(el) {
+                el.focus()
+            }
+        }
+    },
+    methods: {
+        addTodo(){
+            if(this.newTodo.trim().length == 0){
+                return
+            }
+            this.todos.push({
+                id: this.idForTodo,
+                title: this.newTodo,
+                completed: false,
+                editing: false
+            })
+
+        this.newTodo = '';
+        this.idForTodo++;
+        },
+
+        removeTodo(index){
+            this.todos.splice(index, 1)
+        },
+        editTodo(todo){
+            this.beforeEditCache = todo.title
+            todo.editing = true
+        },
+        doneEdit(todo){
+            todo.editing = false;
+        },
+        cancelEdit(todo){
+            todo.title = this.beforeEditCache
+            todo.editing = false;
+
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
+  .todo-item-left { // later
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc; //override defaults
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+    &:hover {
+      background: lightgreen;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .active {
+    background: lightgreen;
+  }
+  // CSS Transitions
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+</style>
